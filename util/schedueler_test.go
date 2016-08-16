6ccc69f2b276ab78fa27daf0be2fa5fafd29d37c
Effi <effoeffi@gmail.com>
2016-08-16 15:01:01 +0300

    Added stop scheduler to test;

--- util/schedueler_test.go
+++ util/schedueler_test.go
@@ -4,21 +4,21 @@ import (
 	"sync"
 	"testing"
 	"time"
-
-	"github.com/stretchr/testify/assert"
 )
 
 func TestSchedulerRun(t *testing.T) {
 	wg := sync.WaitGroup{}
-	wg.Add(1)
-	times := 0
+	wg.Add(2)
 	scheduler := NewScheduler(func() error {
 		defer wg.Done()
-		times++
-
 		return nil
 	}, time.Minute)
-	go func() { scheduler.Run() }()
+	go func() {
+		defer wg.Done()
+		scheduler.Run()
+	}()
+	scheduler.Stop()
+	// Wait validates that scheduler runs the provided method and finish running go routine,
+	// meaning stop scheduler is working (otherwise, test will run forever)
 	wg.Wait()
-	assert.Equal(t, 1, times)
 }
